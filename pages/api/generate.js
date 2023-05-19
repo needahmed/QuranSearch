918e2c925659d0c944b8b5d5cec79298d4b87e15
needahmed <56631873+needahmed@users.noreply.github.com>
2023-05-19 21:23:54 +0500

    Add files via upload

---
+++ pages/api/generate.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `Tell me all the ayats in the quran that either mention or talk about the following, I want all the ayats, do not miss out any :\n`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
+
+  const baseCompletion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 2000,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
